4808d81a2c2178f49528e960dc207a7ee1ad2306
Keeyki <yassine.chtioui34@gmail.com>
2024-10-19 23:41:53 +0200

    work-in-progress-1

--- src/SECEngine.sol
+++ src/SECEngine.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {decentralizeStableCoin} from "src/DecentralizeStableCoin.sol";
+import {ReetrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 /// @title DSCEngine
 /// @author Keeyki
 /// The system is designed to be as minimal as possible, and have the token maintain a 1token = 1 euro peg.
@@ -16,12 +18,21 @@ pragma solidity ^0.8.24;
 ///@notice This contract is the core of the SEC System. It handles all the logic for mining and redeeming DSC tokens,as well as depositing and withdrawing collateral.
 ///@notice This contract is very loosely based on the MakerDAO DSS(DAI) system.
 
-contract DSCEngine {
+contract DSCEngine is ReetrancyGuard {
     /////////////////////
     /// Errors     /////
     ///////////////////
 
-    error DSCEngine_needTobeGreaterThanZero();
+    error SECEngine_needTobeGreaterThanZero();
+    error SECEngine_TokenAddressesAndPricefeedAddressMustBeSameLenght();
+    error SECEngie_NotAllowedToken();
+
+    /////////////////////////
+    /// State Variable /////
+    ///////////////////////
+
+    mapping(address => address) private s_PriceFeeds;
+    decentralizeStableCoin private immutable i_sec;
 
     /////////////////////
     /// Modifiers   ////
@@ -29,14 +40,48 @@ contract DSCEngine {
 
     modifier moreThanZero(uint256 amount) {
         if (amount < 0) {
-            revert DSCEngine_needTobeGreaterThanZero();
+            revert SECEngine_needTobeGreaterThanZero();
         }
         _;
     }
+    modifier isAllowedToken(address token) {
+        if (s_PriceFeeds[token] == address(0)) {
+            revert SECEngie_NotAllowedToken();
+        }
+    }
+
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddress,
+        address secAddress
+    ) {
+        // EUR pricefeed
+        if (tokenAddresses.length != priceFeedAddress.length) {
+            revert SECEngine_TokenAddressesAndPricefeedAddressMustBeSameLenght();
+        }
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_PriceFeeds[tokenAddresses[i]] = priceFeedAddress[i];
+        }
+        i_sec = decentralizeStableCoin(secAddress);
+    }
+
+    /////////////////////
+    /// Functions   ////
+    ///////////////////
+
+    ////////////////////////////
+    /// External Functions ////
+    //////////////////////////
+
     function depositCollateralAndMintSEC(
         address tokenCollateralAddress,
         uint256 amountCollateral
-    ) external moreThanZero(amountCollateral) {
+    )
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReetrant
+    {
         ///@param tokenCollateralAddress The address of the token to deposit as collateral
         ///@param amountCollateral The amount of collateral to deposit
     }
